85717982c62cd28e0c58c72f5ec0183db980fcb4
SILAS-THINKPAD\silas <silasstrawd@ucla.edu>
2025-04-16 08:20:32 -0700

    Error handling for burst integration

--- Non-Spiking/General Functions/integrateBurst.m
+++ Non-Spiking/General Functions/integrateBurst.m
@@ -15,22 +15,27 @@ o     = thisoClass.o;
 % For each neural group, integrate between the peaks
 for ng = 1:length(names) % each neural group
 
-    idx   = thisoClass.peakStruct.(names(ng)).idx;
-    ong = o(:,ng);
+    if ~isempty(thisoClass.peakStruct.(names(ng)).idx)
+        idx   = thisoClass.peakStruct.(names(ng)).idx;
+        ong = o(:,ng);
+        
+        for pk = 1:length(idx)-1 % each peak within a group
+            
+            thisInterval = idx(pk):idx(pk+1); % Grab the indices for the endpoints of the area we're integrating
+            
+            % LH Riemann Sum
+            thisdt = diff(t(thisInterval));      % widths of each partition
+            thiso  = ong(thisInterval(1:end-1)); % left-hand endpoints of each partition
     
-    for pk = 1:length(idx)-1 % each peak within a group
-        
-        thisInterval = idx(pk):idx(pk+1); % Grab the indices for the endpoints of the area we're integrating
-        
-        % LH Riemann Sum
-        thisdt = diff(t(thisInterval));      % widths of each partition
-        thiso  = ong(thisInterval(1:end-1)); % left-hand endpoints of each partition
-
-        thisInt = sum(thisdt.*thiso);
-
-        integralStruct.(names(ng)).outputBurstIntegral(pk) = thisInt;
-        integralStruct.(names(ng)).burstIdx(pk)            = pk;
-
+            thisInt = sum(thisdt.*thiso);
+    
+            integralStruct.(names(ng)).outputBurstIntegral(pk) = thisInt;
+            integralStruct.(names(ng)).burstIdx(pk)            = pk;
+    
+        end
+    else % in case peaks cannot be found
+        integralStruct.(names(ng)).outputBurstIntegral = [];
+        integralStruct.(names(ng)).burstIdx            = [];
     end
 
 end
